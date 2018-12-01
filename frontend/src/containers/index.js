148a3919b1268bbfed50025f5e0e4f0eb2747237
m3n0r <wesleysd1989@gmail.com>
2018-12-01 11:25:16 -0200

    add container CategoriesForm

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -1,5 +1,6 @@
 import Companies from './Companies';
 import CompaniesForm from './CompaniesForm';
 import Categories from './Categories';
+import CategoriesForm from './CategoriesForm';
 
-export { Companies, CompaniesForm, Categories };
+export { Companies, CompaniesForm, Categories, CategoriesForm };
