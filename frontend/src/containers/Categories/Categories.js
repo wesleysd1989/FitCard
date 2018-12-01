d29a2745c1c7a7279fae5300a3b7f382af96bd08
m3n0r <wesleysd1989@gmail.com>
2018-12-01 10:46:25 -0200

    Create Container Categories

---
+++ frontend/src/containers/Categories/Categories.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { Nav } from '../../views'
+
+
+
+class Categories extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />               
+      </div>
+    );
+  }
+}
+
+export default Categories
+
