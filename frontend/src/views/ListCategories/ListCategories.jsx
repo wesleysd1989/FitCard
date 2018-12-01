7a7752bf54b5cee2b57b9e3d7ed961a8287c2f14
m3n0r <wesleysd1989@gmail.com>
2018-12-01 11:07:03 -0200

    button new category

--- frontend/src/views/ListCategories/ListCategories.jsx
+++ frontend/src/views/ListCategories/ListCategories.jsx
@@ -11,6 +11,18 @@ class ListCategories extends Component {
                         <li className="breadcrumb-item active" aria-current="page">Categorias</li>
                     </ol>
                 </nav>
+                <div className="row mb-4">
+                    <div className="col-md">
+                        <h1 className="h2 border-left pl-2">
+                            Categorias
+                        </h1>
+                    </div>
+                    <div className="col-md">
+                        <a href="#/new" className="btn btn-success float-right">
+                            + Nova Categoria
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
