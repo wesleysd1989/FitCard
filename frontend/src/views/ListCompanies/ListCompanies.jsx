df6f34de240701c3962d9661249093ea2f66b7f6
m3n0r <wesleysd1989@gmail.com>
2018-12-01 11:02:22 -0200

    Table List on component

--- frontend/src/views/ListCompanies/ListCompanies.jsx
+++ frontend/src/views/ListCompanies/ListCompanies.jsx
@@ -22,6 +22,52 @@ class ListCompanies extends Component {
                         </a>
                     </div>
                 </div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr className="bg-primary text-light">
+                            <th>Empresa</th>
+                            <th>Categoria</th>
+                            <th className="text-center">Situação</th>
+                            <th className="text-center">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr >
+                            <td>
+                                <strong>Nome da Empresa Aqui</strong><br />
+                                <small className="text-success">10:20 12/12/2012</small><br />
+                                <small >Nome Fantasia</small>
+                            </td>
+                            <td >
+                                Categoria Aqui
+                                    </td>
+                            <td className="text-center">
+                                Status Aqui
+                                    </td>
+                            <td className="text-center">
+                                <a href="#/:id/edit" className="btn btn-outline-info btn-sm mr-2">Editar</a>
+                                <button href="#/:id/delete" className="btn btn-outline-danger btn-sm">Excluir</button>
+                            </td>
+                        </tr>
+                        <tr >
+                            <td>
+                                <strong>Nome da Empresa Aqui</strong><br />
+                                <small className="text-success">10:20 12/12/2012</small><br />
+                                <small >Nome Fantasia</small>
+                            </td>
+                            <td >
+                                Categoria Aqui
+                                    </td>
+                            <td className="text-center">
+                                Status Aqui
+                                    </td>
+                            <td className="text-center">
+                                <a href="#/:id/edit" className="btn btn-outline-info btn-sm mr-2">Editar</a>
+                                <button href="#/:id/delete" className="btn btn-outline-danger btn-sm">Excluir</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
