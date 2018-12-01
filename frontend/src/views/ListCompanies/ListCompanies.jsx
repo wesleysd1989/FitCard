a3541bcce69f88c7f2ba94f203a071c7af28cdde
m3n0r <wesleysd1989@gmail.com>
2018-12-01 10:41:30 -0200

    breadcrumb on listcompanies

--- frontend/src/views/ListCompanies/ListCompanies.jsx
+++ frontend/src/views/ListCompanies/ListCompanies.jsx
@@ -4,8 +4,12 @@ import React, { Component } from 'react'
 class ListCompanies extends Component {
     render() {
         return (
-            <div>
-                ListCompanies
+            <div className="container mb-5">
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item active" aria-current="page">Empresas</li>
+                    </ol>
+                </nav>
             </div>
         )
     }
