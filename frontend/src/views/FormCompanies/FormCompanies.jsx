b1645c4c8955686897e7335b5dafeb77421d9124
m3n0r <wesleysd1989@gmail.com>
2018-12-03 06:02:51 -0200

    initial check form

--- frontend/src/views/FormCompanies/FormCompanies.jsx
+++ frontend/src/views/FormCompanies/FormCompanies.jsx
@@ -6,7 +6,25 @@ class FormCompanies extends Component {
         super(props)
         this.state = {
             ativa: "active",
-            inativa: null
+            inativa: null,
+            verified: false,
+            socialNameCheck1: false,
+            socialNameCheck2: false,
+            cnpjCheck: false,
+            telephoneChecked: false,
+            socialNameValue: "",
+            cnpjValue: "",
+            telephoneValue: "",
+            categoryValue: ""
+            
+
+        }
+    }
+    check(){
+        if (this.state.socialNameValue !== "" && this.state.cnpjValue !== ""){
+            this.setState({ verified: true })
+        } else {
+            this.setState({ verified: false })
         }
     }
     changeState(valor) {
@@ -15,7 +33,72 @@ class FormCompanies extends Component {
         else if (valor === "inativa")
             this.setState({ ativa: null, inativa: "active" })
     }
+
+    changeStateSocialName(value) {
+        console.log(this.state.socialNameValue)
+        this.check()
+        this.setState({ socialNameValue: value })
+        if(value === ""){
+            this.setState({ socialNameCheck1: true })
+        }
+        else{
+            this.setState({ socialNameCheck1: false })
+        }
+        if (value.length <= 1 && value.length > 0){
+            this.setState({ socialNameCheck2: true })
+            this.setState({ socialNameCheck1: false })
+        }
+        else {
+            this.setState({ socialNameCheck2: false })
+        }
+    }
+
+    changeStateCNPJ(value) {
+        this.check()
+        this.setState({ cnpjValue: value })
+        console.log("funcao: " + this.state.cnpjValue)
+        if(value === ""){
+            this.setState({ cnpjCheck: true })
+        }
+        else{
+            this.setState({ cnpjCheck: false })
+        }
+    }
+
+    changeStateCategory(value) {
+        this.check()
+        this.setState({ categoryValue: value })
+        if(value === "Categoria 2" && this.state.telephoneValue === ""){
+            this.setState({ telephoneChecked: true })
+        }
+        else{
+            this.setState({ telephoneChecked: false })
+        }
+    }
+
+    changeStateTelephone(value) {
+        this.check()
+        this.setState({ telephoneValue: value })
+        if(value === "" && this.state.categoryValue === "Categoria 2"){
+            this.setState({ telephoneChecked: true })
+        }
+        else{
+            this.setState({ telephoneChecked: false })
+        }
+    }
+
+    onVerify() {
+        this.setState({
+            verified: true
+        })
+    }
+
     render() {
+        console.log("render: " + this.state.cnpjValue)
+        const socialNameCheck1 =  this.state.socialNameCheck1
+        const socialNameCheck2 =  this.state.socialNameCheck2
+        const cnpjCheck =  this.state.cnpjCheck
+        const telephoneChecked =  this.state.telephoneChecked
         return (
             <div className="container">
                 <nav aria-label="breadcrumb">
@@ -58,8 +141,12 @@ class FormCompanies extends Component {
                             </div>
                             <div className="form-row">
                                 <div className="form-group col-md-12">
-                                    <label htmlFor="socialName">Razão Social</label>
-                                    <input type="text" className="form-control" id="socialName" formcontrolname="socialName" />
+                                    <label htmlFor="socialName">*Razão Social</label>
+                                    <input type="text"  onChange={(e) => this.changeStateSocialName(e.target.value)} className="form-control" id="socialName" formcontrolname="socialName" />
+                                    <div className="text-danger" >
+                                        {!!socialNameCheck1 ? <div >dado obrigatório</div> : <div></div> }
+                                        {!!socialNameCheck2 ? <div >deve ter no mínimo 2 caracteres</div> : <div></div> }
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-row">
@@ -70,12 +157,15 @@ class FormCompanies extends Component {
                             </div>
                             <div className="form-row">
                                 <div className="form-group col-md-4">
-                                    <label htmlFor="cnpj">CNPJ</label>
-                                    <input type="text" className="form-control text-right" id="cnpj" formcontrolname="cnpj" />
+                                    <label htmlFor="cnpj">*CNPJ</label>
+                                    <input type="text" onChange={(e) => this.changeStateCNPJ(e.target.value)} className="form-control text-right" id="cnpj" formcontrolname="cnpj" />
+                                    <div className="text-danger">
+                                        {!!cnpjCheck ? <div >dado obrigatório</div> : <div></div> }
+                                    </div>
                                 </div>
                                 <div className="form-group col-md-3">
                                     <label htmlFor="categoryId">Categoria</label>
-                                    <select name="categoryId" id="categoryId" formcontrolname="categoryId" className="form-control">
+                                    <select name="categoryId" id="categoryId" onChange={(e) => this.changeStateCategory(e.target.value)} formcontrolname="categoryId" className="form-control">
                                         <option>
                                             Categoria 1
                                         </option>
@@ -103,7 +193,10 @@ class FormCompanies extends Component {
                                 </div>
                                 <div className="form-group col-md-2">
                                     <label htmlFor="telephone">Telefone</label>
-                                    <input type="text" className="form-control text-right" id="telephone" formcontrolname="telephone" />
+                                    <input type="text" onChange={(e) => this.changeStateTelephone(e.target.value)} className="form-control text-right" id="telephone" formcontrolname="telephone" />
+                                    <div className="text-danger" >
+                                        {!!telephoneChecked ? <div >dado obrigatório</div> : <div></div> }
+                                    </div>
                                 </div>
                                 <div className="form-group col-md-2" >
                                     <label htmlFor="registerDate">Data Criação</label>
@@ -126,7 +219,11 @@ class FormCompanies extends Component {
                             </div>
                         </div>
                     </div>
-                    <button type="submit" className="btn btn-primary btn-lg float-right mt-3">Salvar</button>
+                    <div className="row">
+                    <div className="col-md-6 mt-3"> - Campos com * devem ser obrigatorios</div>
+                    <div className="col-md-6"><button type="submit" disabled={!this.state.verified} className="btn btn-primary btn-lg float-right mt-3">Salvar</button></div>
+                    </div>
+                    
                 </form>
             </div>
         )
