b3f9c4e3bdae26e29cfee3d961ff3ead27e69940
m3n0r <wesleysd1989@gmail.com>
2018-12-05 03:39:10 -0200

    company schema

---
+++ backend/src/api/schema/company.js
@@ -0,0 +1,20 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const companySchema = new mongoose.Schema({
+  socialName: { type: String, required: true, min: 2},
+  fantasyName: { type: String, required: false},
+  cnpj: { type: String,  required: true },
+  email: { type: String,  required: false },
+  address: { type: String,  required: false },
+  city: { type: String,  required: false },
+  province: { type: String,  required: false},
+  telephone: { type: String,  required: false },
+  registerDate: { type: String,  required: false },
+  status: { type: Boolean,  required: true},
+  agency: { type: String,  required: false },
+  acount: { type: String,  required: false },
+  categoryId: { type: String,  required: false },
+  category: { type: String,  required: false }
+})
+module.exports = restful.model('Company', companySchema)
