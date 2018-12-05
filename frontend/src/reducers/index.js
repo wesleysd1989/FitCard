cf7542ed143af0a0df5bf65fb316a24f1e463502
wesley dos santos dias <wesleysd1989@gmail.com>
2018-12-05 19:12:10 -0200

    create reducer

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+//import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
+
+import Edit from '../edit/EditReducer'
+
+
+
+export default combineReducers({
+    //form: formReducer,
+    toastr: toastrReducer,
+    edit: Edit
+})
