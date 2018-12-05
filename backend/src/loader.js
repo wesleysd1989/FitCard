5cd39ac794f2d03c4783ede55f37e565ad6c4fb9
m3n0r <wesleysd1989@gmail.com>
2018-12-05 03:10:53 -0200

    routes.js

--- backend/src/loader.js
+++ backend/src/loader.js
@@ -1,2 +1,3 @@
 const server = require('./config/server')//importa a configuracao inicial do server
 require('./config/database') //importa a configuracao do banco de dados
+require('./config/routes')(server)//importa a configuracao das rotas passando os paremtros do server
