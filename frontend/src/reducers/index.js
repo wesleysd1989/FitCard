6bdc135bcb869ced10f38696486ec079fe7e9f97
m3n0r <wesleysd1989@gmail.com>
2018-12-05 23:46:04 -0200

    add redux form

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-//import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import Edit from '../edit/EditReducer'
@@ -7,7 +7,7 @@ import Edit from '../edit/EditReducer'
 
 
 export default combineReducers({
-    //form: formReducer,
+    form: formReducer,
     toastr: toastrReducer,
     edit: Edit
 })
