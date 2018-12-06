032bd6e59e7c60d2ad0f1eabdc847d34eb496dab
wesley dos santos dias <wesleysd1989@gmail.com>
2018-12-06 20:15:04 -0200

    Category actions redux

---
+++ frontend/src/category/CategoryActions/CategoryActions.js
@@ -0,0 +1,41 @@
+
+import { toastr } from 'react-redux-toastr'
+import axios from 'axios'
+const URL = 'http://localhost:3003/api';
+export function registerCategory(values) {
+    return submit(values, `${URL}/categories`)
+}
+export function updateCategory(values) {
+    return change(values, `${URL}/categories/${values._id}`)
+}
+function change(values, url) {
+    return dispatch => {
+        axios.put(url, values)
+            .then(resp => {
+                dispatch([
+                    { type: 'CATEGORY', payload: resp.data }
+                ])
+                    toastr.success('Sucesso', "Item Editado com sucesso")
+                
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(
+                   error => toastr.error('Erro', error))
+            })
+    }
+}
+function submit(values, url) {
+    return dispatch => {
+        axios.post(url, values)
+            .then(resp => {
+                dispatch([
+                    { type: 'CATEGORY', payload: resp.data }
+                ])
+                toastr.success('Sucesso', "Item Cadastrado com sucesso")
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(
+                    error => toastr.error('Erro', error))
+            })
+    }
+}
